4f11d92a95068c4bea06285544e0bef674467f88
ldynia <l_dynowski@yahoo.com>
2017-04-21 13:47:27 -0500

    Adding error 500

--- chunked_upload/constants.py
+++ chunked_upload/constants.py
@@ -6,6 +6,7 @@ class http_status:
     HTTP_400_BAD_REQUEST = 400
     HTTP_403_FORBIDDEN = 403
     HTTP_410_GONE = 410
+    HTTP_500_SERVER_ERROR = 500
 
 UPLOADING = 'uploading'
 COMPLETE = 'complete'
